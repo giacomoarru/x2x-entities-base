4fd3253f72c7b4360c39b02f4896c6ef8f424ab5
garru <garru@irishtimes.com>
2020-02-12 05:18:55 +0000

    Amend IDGenericEntity

--- base/src/main/java/cloud/x2x/entities/base/generic/IDGenericEntity.java
+++ base/src/main/java/cloud/x2x/entities/base/generic/IDGenericEntity.java
@@ -12,11 +12,50 @@ import javax.validation.constraints.NotNull;
  *
  */
 @MappedSuperclass
-public class IDGenericEntity {
+public abstract class IDGenericEntity {
 
 	@NotNull
 	@Id
 	@GeneratedValue
 	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		IDGenericEntity other = (IDGenericEntity) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "IDGenericEntity [id=" + id + "]";
+	}
 	
 }
